928573cd86d4b28e86666da4f11a1b4681b2c5b8
Christof Meyer <christof.meyer@anivo.ch>
2016-05-20 22:27:40 +0200

    yo angular & bower install heatmap.js-amd

---
+++ app/scripts/app.js
@@ -0,0 +1,14 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name gletscherelfApp
+ * @description
+ * # gletscherelfApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('gletscherelfApp', [
+    'ngResource'
+  ]);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name gletscherelfApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the gletscherelfApp
+ */
+angular.module('gletscherelfApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
