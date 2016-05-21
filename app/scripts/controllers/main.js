00b8f7b53393b271754e84e5dfdf8d58fb708709
Christof Meyer <christof.meyer@anivo.ch>
2016-05-21 17:12:12 +0200

    final

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -12,9 +12,6 @@ angular.module('gletscherelfApp', ['heatmap', 'ya.nouislider'])
 
     $scope.gameData = {};
 
-
-
-
     function generateRandomData(len) {
       var max = 100;
       var min = 1;
@@ -33,16 +30,8 @@ angular.module('gletscherelfApp', ['heatmap', 'ya.nouislider'])
         max: max,
         min: min,
         data: data
-      }
+      };
     }
-
-    $http.get('data/chrischi.json')
-      .then(function(result) {
-
-        $scope.gameData = result.data.gameData;
-        $scope.heatmapData = $scope.gameData;
-
-      });
 
 
     $scope.heatmapConfig = {
@@ -74,7 +63,7 @@ angular.module('gletscherelfApp', ['heatmap', 'ya.nouislider'])
     // then, create a range slider comprising 90 minutes using above legend values
     var that = this;
     that.options = {
-      start: [0, 15],
+      start: [0, 2],
       behaviour: 'drag',
       step: 1,
       connect: true,
@@ -103,11 +92,11 @@ angular.module('gletscherelfApp', ['heatmap', 'ya.nouislider'])
       return partialGameData;
     }
 
-    function updateDistanceAndSpeed(fromMinute, toMinute) {
-      $scope.distancePeriod = $scope.gameData.data[toMinute].totalDistance - $scope.gameData.data[fromMinute].totalDistance;
-      $scope.distanceTotal = $scope.gameData.data[toMinute].totalDistance;
-      $scope.speedPeriod = $scope.distancePeriod / (toMinute - fromMinute) * 60;
-      $scope.speedTotal = $scope.distanceTotal / toMinute * 60;
+    function updateDistanceAndSpeed(fromSecond, toSecond) {
+      $scope.distancePeriod = $scope.gameData.data[toSecond].totalDistance - $scope.gameData.data[fromSecond].totalDistance;
+      $scope.distanceTotal = $scope.gameData.data[toSecond].totalDistance;
+      $scope.speedPeriod = $scope.distancePeriod / (toSecond - fromSecond) * 3.6;
+      $scope.speedTotal = $scope.distanceTotal / toSecond * 3.6;
 
     }
 
@@ -115,12 +104,26 @@ angular.module('gletscherelfApp', ['heatmap', 'ya.nouislider'])
       var fromMinute = that.options.start[0];
       var toMinute = that.options.start[1];
       $scope.heatmapData = getPartialGameData(fromMinute, toMinute);
-      updateDistanceAndSpeed(fromMinute, toMinute);
+      updateDistanceAndSpeed(fromMinute * 60, toMinute * 60);
     }
 
     that.events = {
       update: updateHeatmap.bind(undefined)
     };
+
+    $scope.loadData = function (file) {
+      $http.get('data/' + file)
+        .then(function(result) {
+
+          $scope.gameData = result.data.gameData;
+          $scope.heatmapData = $scope.gameData;
+          updateHeatmap();
+
+        });
+    };
+
+
+
 
     // to calculate current position
     // $scope.currentMinute = (that.options.start[0] + that.options.start[1]) / 2;
